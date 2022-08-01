d74623a7a82a92be26e0bacdaf4d1ca1844fa3f2
CuongNguyen13 <67322685+CuongNguyen13@users.noreply.github.com>
2022-08-02 02:25:39 +0700

    validate register

--- hatshop-fe/src/componentsHomePage/Register.js
+++ hatshop-fe/src/componentsHomePage/Register.js
@@ -8,73 +8,106 @@ import axios from 'axios';
 
 
 function Register() {
-    // redirect sang trang trước đó navigate(-1)
-    const navigate = useNavigate();
-    window.scrollTo(0, 0);
-    
-    const [check, setCheck] = useState(false);
-    const CheckEmailExist = (email, values) => {
-      registerApi.checkEmailExist(email).then(res => {
-        
-        console.log("res",check)
+  // redirect sang trang trước đó navigate(-1)
+  const navigate = useNavigate();
+  window.scrollTo(0, 0);
+
+  const [check, setCheck] = useState(false);
+
+
+  const [checkEmail, setCheckEmail] = useState("");
+
+  useEffect(() => {
+    console.log("hello")
+    registerApi.checkEmailExist(checkEmail).then(res => {
+      console.log(checkEmail)
+      if (res) {
         setCheck(res)
-        if (res) {
-          setCheck(res)
-        } else {
-          console.log(values, "Dk")
-          registerApi.createRegister(values).then(res => {
-              console.log(res)
-              sessionStorage.setItem("userName", res)
-              registerApi.getId(email).then(res => {
-                console.log("Id", res)
-                sessionStorage.setItem("id", res)
-            }).catch(e => {
-                console.log(e)
-            });
+      } else {
+        
+      }
+    }
+    ).catch(e => {
+      console.log("e", e)
+    });
+  }, [checkEmail])
+
+
+
+
+
+  const CheckEmailExist = (email, values) => {
+    registerApi.checkEmailExist(email).then(res => {
+
+      console.log("res", check)
+      setCheck(res)
+      if (res) {
+        setCheck(res)
+      } else {
+        console.log(values, "Dk")
+        registerApi.createRegister(values).then(res => {
+          console.log(res)
+          sessionStorage.setItem("userName", res)
+          registerApi.getId(email).then(res => {
+            console.log("Id", res)
+            sessionStorage.setItem("id", res)
           }).catch(e => {
-              console.log(e)
+            console.log(e)
           });
+        }).catch(e => {
+          console.log(e)
+        });
         navigate("/")
         alert("Bạn đã đăng ký thành công.");
-        }
-      }).catch(e => {
-          console.log("e",e)
-      });
+      }
+    }).catch(e => {
+      console.log("e", e)
+    });
   }
 
-   
-    //sử lý form
-    const formik = useFormik({
-      
-      initialValues:{
-        fistName : "",
-        lastName : "",
-        email : "",
-        pass : "",
-        province : "",
-        address : "",
-        date : ""
-      },
-      validationSchema: Yup.object({
-        fistName: Yup.string().required("Tên không được trống!").min(2, "Tên tối thiểu 2 ký tự!").matches(/^[a-zA-Z\s]*$/, "Tên không được là số"),
-        lastName: Yup.string().required("Họ và tên đệm không được trống!").min(2, "Họ và tên đệm tối thiểu 2 ký tự!").matches(/^[a-zA-Z\s]*$/, "Họ không được là số"),
-        email : Yup.string().required("Email không được trống!").matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "chưa phù hợp định dạng email!"),
-        pass: Yup.string().required("Mật khẩu không được trống!").min(8, "Mật khẩu tối thiểu từ 8 ký tự!"),
-        province: Yup.string().required("Quê quán không được trống!").min(3, "Nguyên quán tối thiểu 3 ký tự!"),
-        address: Yup.string().required("Địa chỉ không được trống!").min(3, "Địa chỉ tối thiểu 3 ký tự!"),
-      }),
-     
-      onSubmit: (values) => {
+
+  //sử lý form
+  const formik = useFormik({
+
+    initialValues: {
+      fistName: "",
+      lastName: "",
+      email: "",
+      pass: "",
+      province: "",
+      address: "",
+      date: ""
+    },
+    validationSchema: Yup.object({
+      fistName: Yup.string().required("Tên không được trống!").min(2, "Tên tối thiểu 2 ký tự!").matches(/^[a-zA-Z\s]*$/, "Tên không được là số"),
+      lastName: Yup.string().required("Họ và tên đệm không được trống!").min(2, "Họ và tên đệm tối thiểu 2 ký tự!").matches(/^[a-zA-Z\s]*$/, "Họ không được là số"),
+      email: Yup.string().required("Email không được trống!").matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "chưa phù hợp định dạng email!"),
+      pass: Yup.string().required("Mật khẩu không được trống!").min(8, "Mật khẩu tối thiểu từ 8 ký tự!"),
+      province: Yup.string().required("Quê quán không được trống!").min(3, "Nguyên quán tối thiểu 3 ký tự!"),
+      address: Yup.string().required("Địa chỉ không được trống!").min(3, "Địa chỉ tối thiểu 3 ký tự!"),
+    }),
+
+    onSubmit: (values) => {
+      if (check) {
+        alert("Vui lòng kiểm tra lại Email của bạn!!")
+      } else {
         CheckEmailExist(values.email, values)
-        }
-    })
+      }
+    }
+  })
 
+  function checkEmailHandle(email1) {
+    setCheckEmail(email1);
+    console.log(checkEmail)
+    const newInput = { ...formik.values, email:email1 }
+    formik.setValues(newInput)
+    
+  }
 
+  return (
 
-    return (
-      
-      <div className="wrapper" style={{margin:"150px auto",width:"80%"}}>
-        <div className="form-left">
+    <div className="wrapper" style={{ margin: "150px auto", width: "80%" }}>
+      <div className="form-left">
         <h2 className="text-uppercase">Thông tin</h2>
         <p>
           Tạo tài khoản tại đây chúng tôi sẽ bảo mật tuyệt đối thôi tin tài khoản của quý khách. Cùng với đội ngũ Nhân viên nhiều năm kinh nghiệm sẽ hỗ trợ quý khách một cách tốt nhất.
@@ -87,49 +120,49 @@ function Register() {
           <Link to="/Contact"> <input type="submit" className="account" value="Liên hệ" /></Link>
         </div>
       </div>
-        <form className="form-right" style={{backgroundColor:""}} onSubmit={formik.handleSubmit}>
-          <h2 className="text-uppercase">Đăng ký</h2>
-          <div className="row">
-            <div className="col-sm-6 mb-3">
-              <input type="text" name="fistName" id="first_name" className="input-field"  placeholder='Tên' value={formik.values.fistName} onChange={formik.handleChange}/>
-              <span style={{textAlign:"left", color:"red", margin:"0",fontSize:"15px",float:"left"}}>{formik.errors.fistName}</span>
-            </div>
-            <div className="col-sm-6 mb-3">
-              <input type="text" name="lastName" id="last_name" className="input-field" placeholder='Họ và tên đệm' value={formik.values.lastName} onChange={formik.handleChange}/>
-              <span style={{textAlign:"left", color:"red", margin:"0",fontSize:"15px",float:"left"}}>{formik.errors.lastName}</span>
-            </div>
+      <form className="form-right" style={{ backgroundColor: "" }} onSubmit={formik.handleSubmit}>
+        <h2 className="text-uppercase">Đăng ký</h2>
+        <div className="row">
+          <div className="col-sm-6 mb-3">
+            <input type="text" name="fistName" id="first_name" className="input-field" placeholder='Tên' value={formik.values.fistName} onChange={formik.handleChange} />
+            <span style={{ textAlign: "left", color: "red", margin: "0", fontSize: "15px", float: "left" }}>{formik.errors.fistName}</span>
           </div>
-          <div className="mb-3">
-            <input type="email" className="input-field" name="email" id="email" required placeholder='Email' value={formik.values.email} onChange={formik.handleChange}/>
-            <span style={{textAlign:"left", color:"red", margin:"0",fontSize:"15px",float:"left"}}>{formik.errors.email}</span>
-            <span style={{textAlign:"left", color:"red", margin:"0",fontSize:"15px",float:"left"}}>{check?"Email này đã được đăng ký":""}</span>
+          <div className="col-sm-6 mb-3">
+            <input type="text" name="lastName" id="last_name" className="input-field" placeholder='Họ và tên đệm' value={formik.values.lastName} onChange={formik.handleChange} />
+            <span style={{ textAlign: "left", color: "red", margin: "0", fontSize: "15px", float: "left" }}>{formik.errors.lastName}</span>
           </div>
-          <div className="mb-3">
-            <input type="password" className="input-field input-pass" id="pass" name="pass" required placeholder='Mật khẩu' value={formik.values.pass} onChange={formik.handleChange}/>
-            <span style={{textAlign:"left", color:"red", margin:"0",fontSize:"15px",float:"left"}}>{formik.errors.pass}</span>
-          </div>
-          <div className="mb-3">
-            <input type="text" className="input-field" name="province" id="province" required placeholder='Quê quán' value={formik.values.province} onChange={formik.handleChange}/>
-            <span style={{textAlign:"left", color:"red", margin:"0",fontSize:"15px",float:"left"}}>{formik.errors.province}</span>
-          </div>
-          <div className="mb-3">
-            <input type="text" className="input-field" name="address" id="address" required placeholder='Địa chỉ' value={formik.values.address} onChange={formik.handleChange}/>
-            <span style={{textAlign:"left", color:"red", margin:"0",fontSize:"15px",float:"left"}}>{formik.errors.address}</span>
-          </div>
-          <div className="mb-3">
-            <input type="date" id="date" className="input-field" name="date" required value={formik.values.date} onChange={formik.handleChange}/>
-          </div>
-          <div className="mb-3">
-            <label className="option">tôi đồng ý với<a href="#">các Điều khoản và Điều kiện</a>
-              <input id="option" type="checkbox" defaultChecked />
-              <span className="checkmark" />
-            </label>
-            <span id="agree" style={{color:"red", display:"none", fontSize:"15px"}}>Quý khách phải đồng ý với cái điều khoản!</span>
-          </div>
-          <div className="form-field">
-            <input type="submit" defaultValue="Đăng ký" className="register btn-register" name="register" />
-          </div>
-          {/* {Object.keys(errors).length != 0 && (
+        </div>
+        <div className="mb-3">
+          <input type="email" className="input-field" name="email" id="email" required placeholder='Email' value={formik.values.email} onChange={(event) => checkEmailHandle(event.target.value)} />
+          <span style={{ textAlign: "left", color: "red", margin: "0", fontSize: "15px", float: "left" }}>{formik.errors.email}</span>
+          <span style={{ textAlign: "left", color: "red", margin: "0", fontSize: "15px", float: "left" }}>{check ? "Email này đã được đăng ký" : ""}</span>
+        </div>
+        <div className="mb-3">
+          <input type="password" className="input-field input-pass" id="pass" name="pass" required placeholder='Mật khẩu' value={formik.values.pass} onChange={formik.handleChange} />
+          <span style={{ textAlign: "left", color: "red", margin: "0", fontSize: "15px", float: "left" }}>{formik.errors.pass}</span>
+        </div>
+        <div className="mb-3">
+          <input type="text" className="input-field" name="province" id="province" required placeholder='Quê quán' value={formik.values.province} onChange={formik.handleChange} />
+          <span style={{ textAlign: "left", color: "red", margin: "0", fontSize: "15px", float: "left" }}>{formik.errors.province}</span>
+        </div>
+        <div className="mb-3">
+          <input type="text" className="input-field" name="address" id="address" required placeholder='Địa chỉ' value={formik.values.address} onChange={formik.handleChange} />
+          <span style={{ textAlign: "left", color: "red", margin: "0", fontSize: "15px", float: "left" }}>{formik.errors.address}</span>
+        </div>
+        <div className="mb-3">
+          <input type="date" id="date" className="input-field" name="date" required value={formik.values.date} onChange={formik.handleChange} />
+        </div>
+        <div className="mb-3">
+          <label className="option">tôi đồng ý với<a href="#">các Điều khoản và Điều kiện</a>
+            <input id="option" type="checkbox" defaultChecked />
+            <span className="checkmark" />
+          </label>
+          <span id="agree" style={{ color: "red", display: "none", fontSize: "15px" }}>Quý khách phải đồng ý với cái điều khoản!</span>
+        </div>
+        <div className="form-field">
+          <input type="submit" defaultValue="Đăng ký" className="register btn-register" name="register" />
+        </div>
+        {/* {Object.keys(errors).length != 0 && (
           <ul className='error-container'>
             {errors.name?.type == 'required' && <li>Name is required</li>}
             {errors.pass?.type == 'required' && <li>Password is required</li>}
@@ -137,9 +170,9 @@ function Register() {
           </ul>
           )} */}
 
-        </form>
-      </div>
-    );
+      </form>
+    </div>
+  );
 }
 
 
